94934400d81152b7f3b553107e0f6ef5004b091b
Facundo Fernandez <91030332+facundotfernandez@users.noreply.github.com>
2023-12-06 18:57:16 -0300

    Update localdata.js table

--- scripts/localdata.js
+++ scripts/localdata.js
@@ -1,246 +1,221 @@
 const data = {
-  "expenses": [
+  "cardholders": [
     {
       "_id": 1,
-      "date": "2023-10-22",
+      "name": "Nahuel Astarita",
       "card_number": "XXXX-4444",
-      "cardholder": "Nahuel Astarita (Titular)",
-      "currency": "ARS",
-      "cuit": "12345678901",
-      "business": "Carrefour",
-      "receipt": "12345",
-      "dues": "",
-      "amount": 150.00
+      "expenses": [
+        {
+          "date": "2023-10-22",
+          "currency": "ARS",
+          "cuit": "12345678901",
+          "business": "Carrefour",
+          "receipt": "12345",
+          "dues": "CUOTA 04/04",
+          "amount": 1500.00
+        },
+        {
+          "date": "2023-10-29",
+          "currency": "USD",
+          "cuit": "12345678901",
+          "business": "Fitness+",
+          "receipt": "24680",
+          "dues": "",
+          "amount": 7005.00
+        },
+        {
+          "date": "2023-11-05",
+          "currency": "ARS",
+          "cuit": "56789012345",
+          "business": "Napoli",
+          "receipt": "24684",
+          "dues": "",
+          "amount": 400.00
+        },
+        {
+          "date": "2023-11-08",
+          "currency": "USD",
+          "cuit": "12345678901",
+          "business": "Walmart",
+          "receipt": "24685",
+          "dues": "",
+          "amount": 100.00
+        },
+        {
+          "date": "2023-11-11",
+          "currency": "ARS",
+          "cuit": "12345678901",
+          "business": "Disco",
+          "receipt": "24690",
+          "dues": "",
+          "amount": 1800.00
+        },
+      ]
     },
     {
       "_id": 2,
-      "date": "2023-10-23",
+      "name": "Melisa Colle",
       "card_number": "XXXX-8888",
-      "cardholder": "Melisa Colle",
-      "currency": "ARS",
-      "cuit": "23456789012",
-      "business": "YPF Serviclub",
-      "receipt": "67890",
-      "dues": "CUOTA 01/06",
-      "amount": 50.00
+      "expenses": [
+        {
+          "date": "2023-10-23",
+          "currency": "ARS",
+          "cuit": "23456789012",
+          "business": "YPF Serviclub",
+          "receipt": "67890",
+          "dues": "CUOTA 01/06",
+          "amount": 5000.00
+        },
+        {
+          "date": "2023-11-01",
+          "currency": "ARS",
+          "cuit": "23456789012",
+          "business": "Farmacity",
+          "receipt": "24681",
+          "dues": "",
+          "amount": 300.00
+        },
+        {
+          "date": "2023-11-08",
+          "currency": "ARS",
+          "cuit": "23456789012",
+          "business": "Fravega",
+          "receipt": "24686",
+          "dues": "CUOTA 03/06",
+          "amount": 3000.00
+        },
+        {
+          "date": "2023-11-12",
+          "currency": "ARS",
+          "cuit": "23456789012",
+          "business": "Farmacity",
+          "receipt": "24691",
+          "dues": "",
+          "amount": 35000.00
+        },
+      ]
     },
     {
       "_id": 3,
-      "date": "2023-10-26",
-      "card_number": "XXXX-2222",
-      "cardholder": "Micaela Spadaro",
-      "currency": "ARS",
-      "cuit": "34567890123",
-      "business": "La Central",
-      "receipt": "54321",
-      "dues": "",
-      "amount": 80.00
+      "name": "Facundo Fernandez",
+      "card_number": "XXXX-0000",
+      "expenses": [
+        {
+          "date": "2023-10-27",
+          "currency": "ARS",
+          "cuit": "56789012345",
+          "business": "Fibertel",
+          "receipt": "13579",
+          "dues": "",
+          "amount": 600.00
+        },
+        {
+          "date": "2023-11-11",
+          "currency": "ARS",
+          "cuit": "56789012345",
+          "business": "ARBA",
+          "receipt": "24689",
+          "dues": "CUOTA 01/03",
+          "amount": 1000.00
+        },
+        {
+          "date": "2023-11-20",
+          "currency": "ARS",
+          "cuit": "56789012345",
+          "business": "YPF Serviclub",
+          "receipt": "24694",
+          "dues": "",
+          "amount": 45.00
+        },
+      ]
     },
     {
       "_id": 4,
-      "date": "2023-10-26",
+      "name": "Chiara Papa",
       "card_number": "XXXX-6666",
-      "cardholder": "Chiara Papa",
-      "currency": "ARS",
-      "cuit": "45678901234",
-      "business": "Macowens",
-      "receipt": "98765",
-      "dues": "CUOTA 01/06",
-      "amount": 200.00
+      "expenses": [
+        {
+          "date": "2023-10-26",
+          "currency": "ARS",
+          "cuit": "45678901234",
+          "business": "Macowens",
+          "receipt": "98765",
+          "dues": "CUOTA 01/06",
+          "amount": 2000.00
+        },
+        {
+          "date": "2023-11-02",
+          "currency": "ARS",
+          "cuit": "45678901234",
+          "business": "Cultural",
+          "receipt": "24683",
+          "dues": "CUOTA 02/04",
+          "amount": 5000.00
+        },
+      ]
     },
     {
       "_id": 5,
-      "date": "2023-10-27",
-      "card_number": "XXXX-0000",
-      "cardholder": "Facundo Fernandez",
-      "currency": "ARS",
-      "cuit": "56789012345",
-      "business": "Fibertel",
-      "receipt": "13579",
-      "dues": "",
-      "amount": 60.00
-    },
-    {
-      "_id": 6,
-      "date": "2023-10-29",
-      "card_number": "XXXX-4444",
-      "cardholder": "Nahuel Astarita (Titular)",
-      "currency": "USD",
-      "cuit": "12345678901",
-      "business": "Fitness+",
-      "receipt": "24680",
-      "dues": "",
-      "amount": 75.00
-    },
-    {
-      "_id": 7,
-      "date": "2023-11-01",
-      "card_number": "XXXX-8888",
-      "cardholder": "Melisa Colle",
-      "currency": "ARS",
-      "cuit": "23456789012",
-      "business": "Farmacity",
-      "receipt": "24681",
-      "dues": "",
-      "amount": 30.00
-    },
-    {
-      "_id": 8,
-      "date": "2023-11-02",
+      "name": "Micaela Spadaro",
       "card_number": "XXXX-2222",
-      "cardholder": "Micaela Spadaro",
-      "currency": "USD",
-      "cuit": "34567890123",
-      "business": "Cinepolis",
-      "receipt": "24682",
-      "dues": "",
-      "amount": 15.00
-    },
-    {
-      "_id": 9,
-      "date": "2023-11-02",
-      "card_number": "XXXX-6666",
-      "cardholder": "Chiara Papa",
-      "currency": "ARS",
-      "cuit": "45678901234",
-      "business": "Cultural",
-      "receipt": "24683",
-      "dues": "CUOTA 02/04",
-      "amount": 50.00
-    },
-    {
-      "_id": 10,
-      "date": "2023-11-05",
-      "card_number": "XXXX-0000",
-      "cardholder": "Facundo Fernandez",
-      "currency": "ARS",
-      "cuit": "56789012345",
-      "business": "Napoli",
-      "receipt": "24684",
-      "dues": "",
-      "amount": 40.00
-    },
-    {
-      "_id": 11,
-      "date": "2023-11-08",
-      "card_number": "XXXX-4444",
-      "cardholder": "Nahuel Astarita (Titular)",
-      "currency": "USD",
-      "cuit": "12345678901",
-      "business": "Walmart",
-      "receipt": "24685",
-      "dues": "",
-      "amount": 10.00
-    },
-    {
-      "_id": 12,
-      "date": "2023-11-08",
-      "card_number": "XXXX-8888",
-      "cardholder": "Melisa Colle",
-      "currency": "ARS",
-      "cuit": "23456789012",
-      "business": "Fravega",
-      "receipt": "24686",
-      "dues": "CUOTA 03/06",
-      "amount": 300.00
-    },
-    {
-      "_id": 13,
-      "date": "2023-11-09",
-      "card_number": "XXXX-2222",
-      "cardholder": "Micaela Spadaro",
-      "currency": "ARS",
-      "cuit": "34567890123",
-      "business": "Aroma",
-      "receipt": "24687",
-      "dues": "",
-      "amount": 20.00
-    },
-    {
-      "_id": 14,
-      "date": "2023-11-11",
-      "card_number": "XXXX-6666",
-      "cardholder": "Chiara Papa",
-      "currency": "USD",
-      "cuit": "45678901234",
-      "business": "San Cristobal",
-      "receipt": "24688",
-      "dues": "",
-      "amount": 120.00
-    },
-    {
-      "_id": 15,
-      "date": "2023-11-11",
-      "card_number": "XXXX-0000",
-      "cardholder": "Facundo Fernandez",
-      "currency": "ARS",
-      "cuit": "56789012345",
-      "business": "ARBA",
-      "receipt": "24689",
-      "dues": "CUOTA 01/03",
-      "amount": 100.00
-    },
-    {
-      "_id": 16,
-      "date": "2023-11-11",
-      "card_number": "XXXX-4444",
-      "cardholder": "Nahuel Astarita (Titular)",
-      "currency": "ARS",
-      "cuit": "12345678901",
-      "business": "Disco",
-      "receipt": "24690",
-      "dues": "",
-      "amount": 180.00
-    },
-    {
-      "_id": 17,
-      "date": "2023-11-12",
-      "card_number": "XXXX-8888",
-      "cardholder": "Melisa Colle",
-      "currency": "ARS",
-      "cuit": "23456789012",
-      "business": "Farmacity",
-      "receipt": "24691",
-      "dues": "",
-      "amount": 35.00
-    },
-    {
-      "_id": 18,
-      "date": "2023-11-13",
-      "card_number": "XXXX-2222",
-      "cardholder": "Micaela Spadaro",
-      "currency": "ARS",
-      "cuit": "34567890123",
-      "business": "Walmart",
-      "receipt": "24692",
-      "dues": "",
-      "amount": 85.00
-    },
-    {
-      "_id": 19,
-      "date": "2023-11-15",
-      "card_number": "XXXX-6666",
-      "cardholder": "Chiara Papa",
-      "currency": "ARS",
-      "cuit": "45678901234",
-      "business": "Cultural",
-      "receipt": "24693",
-      "dues": "CUOTA 03/04",
-      "amount": 60.00
-    },
-    {
-      "_id": 20,
-      "date": "2023-11-20",
-      "card_number": "XXXX-0000",
-      "cardholder": "Facundo Fernandez",
-      "currency": "ARS",
-      "cuit": "56789012345",
-      "business": "YPF Serviclub",
-      "receipt": "24694",
-      "dues": "",
-      "amount": 45.00
+      "expenses": [
+        {
+          "date": "2023-10-26",
+          "currency": "ARS",
+          "cuit": "34567890123",
+          "business": "La Central",
+          "receipt": "54321",
+          "dues": "",
+          "amount": 800.00
+        },
+        {
+          "date": "2023-11-02",
+          "currency": "USD",
+          "cuit": "34567890123",
+          "business": "Cinepolis",
+          "receipt": "24682",
+          "dues": "",
+          "amount": 15000.00
+        },
+        {
+          "date": "2023-11-09",
+          "currency": "ARS",
+          "cuit": "34567890123",
+          "business": "Aroma",
+          "receipt": "24687",
+          "dues": "",
+          "amount": 200.00
+        },
+        {
+          "date": "2023-11-11",
+          "currency": "USD",
+          "cuit": "45678901234",
+          "business": "San Cristobal",
+          "receipt": "24688",
+          "dues": "",
+          "amount": 1200.00
+        },
+        {
+          "date": "2023-11-13",
+          "currency": "ARS",
+          "cuit": "34567890123",
+          "business": "Walmart",
+          "receipt": "24692",
+          "dues": "",
+          "amount": 850.00
+        },
+        {
+          "date": "2023-11-15",
+          "currency": "ARS",
+          "cuit": "45678901234",
+          "business": "Cultural",
+          "receipt": "24693",
+          "dues": "CUOTA 03/04",
+          "amount": 6015.00
+        },
+      ]
     }
-  ]
+  ],
 };
 
 export { data }
